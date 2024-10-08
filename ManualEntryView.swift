a78378261e557a2d670370ab099ae5600a22e6a7
Kabir <kabir.rajkotia@gmail.com>
2024-10-08 16:17:06 -0700

    test2

---
+++ ManualEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  ManualEntryView.swift
+//  Label Scanner
+//
+//  Created by Kabir Rajkotia on 9/10/24.
+//
+
+import SwiftUI
+
+struct ManualEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManualEntryView()
+}
---
+++ openAITest.swift
@@ -0,0 +1,18 @@
+//
+//  openAITest.swift
+//  Label Scanner
+//
+//  Created by Kabir Rajkotia on 9/5/24.
+//
+
+import SwiftUI
+
+struct openAITest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    openAITest()
+}
